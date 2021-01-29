5bda2eae2466793c1328bcc63de3d8edc77013b1
nitish kumar <64907230+nitish174@users.noreply.github.com>
2021-01-29 15:56:31 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Footer =()=>{
+    return (
+        <>
+
+        <footer className="bg-light text-center">
+<p>hi there</p>
+        </footer>
+        </>
+
+    )
+};
+export default Footer;
